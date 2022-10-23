af29be799f178a838835dcc8ddf3aafffc0d3ca1
Dev <devendra.aaru@outlook.com>
2022-10-23 10:10:56 -0400

    remove build generated
    Signed-off-by: Dev <devendra.aaru@outlook.com>

--- code/types/code/target/debug/deps/types-501339a9ff614e2b.d
+++
@@ -1,5 +0,0 @@
-/home/devnaga/github/rust_learning/types/target/debug/deps/types-501339a9ff614e2b: src/main.rs
-
-/home/devnaga/github/rust_learning/types/target/debug/deps/types-501339a9ff614e2b.d: src/main.rs
-
-src/main.rs:
--- code/types/code/target/debug/types.d
+++
@@ -1,1 +0,0 @@
-/home/devnaga/github/rust_learning/types/target/debug/types: /home/devnaga/github/rust_learning/types/src/main.rs
